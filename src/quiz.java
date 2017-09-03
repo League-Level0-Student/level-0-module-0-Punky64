6e09db6e244a7f069fe8860a96e94fdd65e5c0a8
Punky64 <league@WTS-iMac-11.attlocal.net>
2017-09-03 15:58:47 -0700

    stuff

---
+++ src/quiz.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+
+public class quiz {
+	public static void main(String[] args) {
+		String Spiders = JOptionPane.showInputDialog("I bet you don't know how many legs does a spider has. But Lets see if you can guess");
+		if (Spiders.equals("8")) {
+			JOptionPane.showInputDialog("... C-Corect");
+
+		} else if (Spiders.matches("[0-9]+")) {
+			JOptionPane.showInputDialog("Ha no but I'll give you another chance");
+
+		}else if (Spiders.equals("eight")) {
+			JOptionPane.showInputDialog("... C-Corect");
+		}else{ JOptionPane.showInputDialog("Ha no but I'll give you another chance");
+		}
+		}
+
+	}
+
---
+++ src/riddles.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+public class riddles {
+
+	public static void main(String[] args) {
+
+		// 1. Make a variable to hold the score
+        int score= 0;
+		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
+        JOptionPane.showInputDialog("When I eat I grow")
+		// 4. If they got the answer right, pop up "correct!" and increase the score by one
+
+		// 5. Otherwise, say "wrong" and tell them the answer
+
+		// 6. Add some more riddles
+
+		// 2. Make a pop up to show the score.
+		JOptionPane.showMessageDialog(null, "score 0");
+	}
+}
+
